21e736f5e462f52df6dc101c7bd14d72004e6891
Waldo Espinoza <waldo@whathecode.com>
2024-06-11 14:11:31 -0600

    Feat: Se agrego controlador para recuperar estados

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 export * from './file-download.controller';
 export * from './file-upload.controller';
+export * from './location.controller';
 export * from './ping.controller';
 export * from './role.controller';
 export * from './user.controller';
-
