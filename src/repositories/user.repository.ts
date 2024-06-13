fb088461f7d89f7aabebb4d8e2402d91d92c9864
Waldo Espinoza <waldo@whathecode.com>
2024-06-12 19:24:24 -0600

    feat: Relacion de usuario hacia branch

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,13 +3,14 @@ import {BelongsToAccessor, HasOneRepositoryFactory, repository} from '@loopback/
 import {DbDataSource} from '../datasources/db.datasource';
 import {LogModelName} from '../enums';
 import {OperationHookBindings} from '../keys';
-import {Organization, Role, User, UserCredentials, UserData, UserRelations} from '../models';
+import {Organization, Role, User, UserCredentials, UserData, UserRelations, Branch} from '../models';
 import {OperationHook} from '../operation-hooks';
 import {OrganizationRepository} from './organization.repository';
 import {RoleRepository} from './role.repository';
 import {SoftCrudRepository} from './soft-delete-entity.repository.base';
 import {UserCredentialsRepository} from './user-credentials.repository';
 import {UserDataRepository} from './user-data.repository';
+import {BranchRepository} from './branch.repository';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class UserRepository extends SoftCrudRepository<
@@ -29,17 +30,21 @@ export class UserRepository extends SoftCrudRepository<
 
   public readonly immediateBoss: BelongsToAccessor<User, typeof User.prototype.id>;
 
+  public readonly branch: BelongsToAccessor<Branch, typeof User.prototype.id>;
+
   constructor(
     //@inject('models.User') model: User,
     @inject('datasources.db') dataSource: DbDataSource,
     @inject.getter(OperationHookBindings.OPERATION_SERVICE)
     public operationHook: Getter<OperationHook>,
-    @repository.getter('UserCredentialsRepository') protected userCredentialsRepositoryGetter: Getter<UserCredentialsRepository>, @repository.getter('RoleRepository') protected roleRepositoryGetter: Getter<RoleRepository>, @repository.getter('OrganizationRepository') protected organizationRepositoryGetter: Getter<OrganizationRepository>, @repository.getter('UserDataRepository') protected userDataRepositoryGetter: Getter<UserDataRepository>, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
+    @repository.getter('UserCredentialsRepository') protected userCredentialsRepositoryGetter: Getter<UserCredentialsRepository>, @repository.getter('RoleRepository') protected roleRepositoryGetter: Getter<RoleRepository>, @repository.getter('OrganizationRepository') protected organizationRepositoryGetter: Getter<OrganizationRepository>, @repository.getter('UserDataRepository') protected userDataRepositoryGetter: Getter<UserDataRepository>, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>, @repository.getter('BranchRepository') protected branchRepositoryGetter: Getter<BranchRepository>,
   ) {
     super(
       User,
       dataSource
     );
+    this.branch = this.createBelongsToAccessorFor('branch', branchRepositoryGetter,);
+    this.registerInclusionResolver('branch', this.branch.inclusionResolver);
     this.immediateBoss = this.createBelongsToAccessorFor('immediateBoss', userRepositoryGetter,);
     this.registerInclusionResolver('immediateBoss', this.immediateBoss.inclusionResolver);
 
