2620c4c106593cbfa70bb91628ba4f80ab33c2d7
Waldo Espinoza <waldo@whathecode.com>
2024-06-20 10:00:04 -0600

    Feat:Sangria

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,20 +1,20 @@
 import {authenticate} from '@loopback/authentication';
 import {inject} from '@loopback/core';
 import {
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where
+    CountSchema,
+    Filter,
+    FilterExcludingWhere,
+    repository,
+    Where
 } from '@loopback/repository';
 import {
-  get,
-  getModelSchemaRef,
-  param,
-  patch,
-  post,
-  requestBody,
-  response
+    get,
+    getModelSchemaRef,
+    param,
+    patch,
+    post,
+    requestBody,
+    response
 } from '@loopback/rest';
 import {CustomerServiceBindings} from '../keys';
 import {Customer} from '../models';
@@ -23,94 +23,94 @@ import {CustomerService} from '../services';
 
 @authenticate('jwt')
 export class CustomerController {
-  constructor(
-    @repository(CustomerRepository)
-    public customerRepository: CustomerRepository,
-    @inject(CustomerServiceBindings.CUSTOMER_SERVICE)
-    public customerService: CustomerService
-  ) { }
+    constructor(
+        @repository(CustomerRepository)
+        public customerRepository: CustomerRepository,
+        @inject(CustomerServiceBindings.CUSTOMER_SERVICE)
+        public customerService: CustomerService
+    ) { }
 
-  @post('/customers')
-  @response(200, {
-    description: 'Customer model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Customer)}},
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Customer, {
-            title: 'NewCustomer',
-            exclude: ["id", "isDeleted", "createdAt", "createdBy", "updatedBy", "updatedAt", "deleteComment", "organizationId"],
-          }),
-        },
-      },
+    @post('/customers')
+    @response(200, {
+        description: 'Customer model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Customer)}},
     })
-    customer: Omit<Customer, 'id'>,
-  ): Promise<object> {
-    return this.customerService.create(customer);
-  }
+    async create(
+        @requestBody({
+            content: {
+                'application/json': {
+                    schema: getModelSchemaRef(Customer, {
+                        title: 'NewCustomer',
+                        exclude: ["id", "isDeleted", "createdAt", "createdBy", "updatedBy", "updatedAt", "deleteComment", "organizationId"],
+                    }),
+                },
+            },
+        })
+        customer: Omit<Customer, 'id'>,
+    ): Promise<object> {
+        return this.customerService.create(customer);
+    }
 
-  @get('/customers/count')
-  @response(200, {
-    description: 'Customer model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(Customer) where?: Where<Customer>,
-  ): Promise<object> {
-    return this.customerService.count(where);
-  }
-
-  @get('/customers')
-  @response(200, {
-    description: 'Array of Customer model instances',
-    content: {
-      'application/json': {
-        schema: {
-          type: 'array',
-          items: getModelSchemaRef(Customer, {includeRelations: true}),
-        },
-      },
-    },
-  })
-  async find(
-    @param.filter(Customer) filter?: Filter<Customer>,
-  ): Promise<object> {
-    return this.customerService.find(filter);
-  }
-
-  @get('/customers/{id}')
-  @response(200, {
-    description: 'Customer model instance',
-    content: {
-      'application/json': {
-        schema: getModelSchemaRef(Customer, {includeRelations: true}),
-      },
-    },
-  })
-  async findById(
-    @param.path.number('id') id: number,
-    @param.filter(Customer, {exclude: 'where'}) filter?: FilterExcludingWhere<Customer>
-  ): Promise<object> {
-    return this.customerService.findById(id, filter);
-  }
-
-  @patch('/customers/{id}')
-  @response(204, {
-    description: 'Customer PATCH success',
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Customer, {partial: true}),
-        },
-      },
+    @get('/customers/count')
+    @response(200, {
+        description: 'Customer model count',
+        content: {'application/json': {schema: CountSchema}},
     })
-    customer: Customer,
-  ): Promise<void> {
-    await this.customerService.updateById(id, customer);
-  }
+    async count(
+        @param.where(Customer) where?: Where<Customer>,
+    ): Promise<object> {
+        return this.customerService.count(where);
+    }
+
+    @get('/customers')
+    @response(200, {
+        description: 'Array of Customer model instances',
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'array',
+                    items: getModelSchemaRef(Customer, {includeRelations: true}),
+                },
+            },
+        },
+    })
+    async find(
+        @param.filter(Customer) filter?: Filter<Customer>,
+    ): Promise<object> {
+        return this.customerService.find(filter);
+    }
+
+    @get('/customers/{id}')
+    @response(200, {
+        description: 'Customer model instance',
+        content: {
+            'application/json': {
+                schema: getModelSchemaRef(Customer, {includeRelations: true}),
+            },
+        },
+    })
+    async findById(
+        @param.path.number('id') id: number,
+        @param.filter(Customer, {exclude: 'where'}) filter?: FilterExcludingWhere<Customer>
+    ): Promise<object> {
+        return this.customerService.findById(id, filter);
+    }
+
+    @patch('/customers/{id}')
+    @response(204, {
+        description: 'Customer PATCH success',
+    })
+    async updateById(
+        @param.path.number('id') id: number,
+        @requestBody({
+            content: {
+                'application/json': {
+                    schema: getModelSchemaRef(Customer, {partial: true}),
+                },
+            },
+        })
+        customer: Customer,
+    ): Promise<void> {
+        await this.customerService.updateById(id, customer);
+    }
 }
