64dbb141fcbebead659dc6495c7f8c8fd8540f11
Waldo Espinoza <waldo@whathecode.com>
2024-06-24 18:15:24 -0600

    Project

--- src/repositories/project.repository.ts
+++ src/repositories/project.repository.ts
@@ -1,10 +1,9 @@
-import {inject, Getter} from '@loopback/core';
+import {Getter, inject} from '@loopback/core';
+import {BelongsToAccessor, repository} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {Project, ProjectRelations, Quotation, Customer} from '../models';
-import {SoftCrudRepository} from './soft-delete-entity.repository.base';
-import {repository, BelongsToAccessor} from '@loopback/repository';
+import {Project, ProjectRelations, Quotation} from '../models';
 import {QuotationRepository} from './quotation.repository';
-import {CustomerRepository} from './customer.repository';
+import {SoftCrudRepository} from './soft-delete-entity.repository.base';
 
 export class ProjectRepository extends SoftCrudRepository<
   Project,
@@ -14,14 +13,11 @@ export class ProjectRepository extends SoftCrudRepository<
 
   public readonly quotation: BelongsToAccessor<Quotation, typeof Project.prototype.id>;
 
-  public readonly customer: BelongsToAccessor<Customer, typeof Project.prototype.id>;
 
   constructor(
-    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('QuotationRepository') protected quotationRepositoryGetter: Getter<QuotationRepository>, @repository.getter('CustomerRepository') protected customerRepositoryGetter: Getter<CustomerRepository>,
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('QuotationRepository') protected quotationRepositoryGetter: Getter<QuotationRepository>,
   ) {
     super(Project, dataSource);
-    this.customer = this.createBelongsToAccessorFor('customer', customerRepositoryGetter,);
-    this.registerInclusionResolver('customer', this.customer.inclusionResolver);
     this.quotation = this.createBelongsToAccessorFor('quotation', quotationRepositoryGetter,);
     this.registerInclusionResolver('quotation', this.quotation.inclusionResolver);
   }
