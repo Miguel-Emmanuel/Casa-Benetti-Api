8f4c8fabe149842d379639e60a697f48ceab989a
Waldo Espinoza <waldo@whathecode.com>
2024-07-08 15:33:05 -0600

    Save

--- src/repositories/proof-payment-quotation.repository.ts
+++ src/repositories/proof-payment-quotation.repository.ts
@@ -1,11 +1,11 @@
-import {inject, Getter} from '@loopback/core';
-import {DefaultCrudRepository, repository, HasManyRepositoryFactory, BelongsToAccessor} from '@loopback/repository';
+import {Getter, inject} from '@loopback/core';
+import {BelongsToAccessor, DefaultCrudRepository, HasManyRepositoryFactory, repository} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {ProofPaymentQuotation, ProofPaymentQuotationRelations, Document, Quotation, Product, Provider} from '../models';
+import {Document, ProofPaymentQuotation, ProofPaymentQuotationRelations, Quotation} from '../models';
 import {DocumentRepository} from './document.repository';
-import {QuotationRepository} from './quotation.repository';
 import {ProductRepository} from './product.repository';
 import {ProviderRepository} from './provider.repository';
+import {QuotationRepository} from './quotation.repository';
 
 export class ProofPaymentQuotationRepository extends DefaultCrudRepository<
   ProofPaymentQuotation,
@@ -17,18 +17,11 @@ export class ProofPaymentQuotationRepository extends DefaultCrudRepository<
 
   public readonly quotation: BelongsToAccessor<Quotation, typeof ProofPaymentQuotation.prototype.id>;
 
-  public readonly product: BelongsToAccessor<Product, typeof ProofPaymentQuotation.prototype.id>;
-
-  public readonly provider: BelongsToAccessor<Provider, typeof ProofPaymentQuotation.prototype.id>;
 
   constructor(
     @inject('datasources.db') dataSource: DbDataSource, @repository.getter('DocumentRepository') protected documentRepositoryGetter: Getter<DocumentRepository>, @repository.getter('QuotationRepository') protected quotationRepositoryGetter: Getter<QuotationRepository>, @repository.getter('ProductRepository') protected productRepositoryGetter: Getter<ProductRepository>, @repository.getter('ProviderRepository') protected providerRepositoryGetter: Getter<ProviderRepository>,
   ) {
     super(ProofPaymentQuotation, dataSource);
-    this.provider = this.createBelongsToAccessorFor('provider', providerRepositoryGetter,);
-    this.registerInclusionResolver('provider', this.provider.inclusionResolver);
-    this.product = this.createBelongsToAccessorFor('product', productRepositoryGetter,);
-    this.registerInclusionResolver('product', this.product.inclusionResolver);
     this.quotation = this.createBelongsToAccessorFor('quotation', quotationRepositoryGetter,);
     this.registerInclusionResolver('quotation', this.quotation.inclusionResolver);
     this.documents = this.createHasManyRepositoryFactoryFor('documents', documentRepositoryGetter,);
