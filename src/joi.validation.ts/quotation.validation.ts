871899eb8e13d6f1694347f4546296a7dcfa0e09
Waldo Espinoza <waldo@whathecode.com>
2024-07-15 09:07:06 -0600

    location

--- src/joi.validation.ts/quotation.validation.ts
+++ src/joi.validation.ts/quotation.validation.ts
@@ -88,7 +88,7 @@ const products = Joi.object({
     percentageDiscountProduct: Joi.number().required(),
     discountProduct: Joi.number().required(),
     subtotalDiscount: Joi.number().required(),
-    location: Joi.string(),
+    location: Joi.string().allow('').allow(null),
     assembledProducts: Joi.array().items(schemaAssembledProducts).optional().allow(null),
 })
 
