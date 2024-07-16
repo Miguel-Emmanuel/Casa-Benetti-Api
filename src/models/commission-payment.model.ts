020c725faa7e035e81e8a0c3d2c40ebe2dc379de
Waldo Espinoza <waldo@whathecode.com>
2024-07-16 14:53:30 -0600

    model

---
+++ src/models/commission-payment.model.ts
@@ -0,0 +1,54 @@
+import {Entity, model, property} from '@loopback/repository';
+import {CommissionPaymentStatus} from '../enums';
+
+@model()
+export class CommissionPayment extends Entity {
+    @property({
+        type: 'number',
+        id: true,
+        generated: true,
+    })
+    id: number;
+
+    //Fecha de creacion
+    @property({
+        type: 'date',
+        default: () => new Date(),
+    })
+    createdAt: Date;
+
+    //Fecha de pago
+    @property({
+        type: 'date',
+    })
+    paymentDate: Date;
+
+    //Monto
+    @property({
+        type: 'number',
+        required: false,
+        postgresql: {
+            dataType: 'double precision',
+        },
+    })
+    amount: number;
+
+    //Estatus del pago
+    @property({
+        type: 'string',
+        required: false,
+        default: CommissionPaymentStatus.PENDIENTE
+    })
+    status: CommissionPaymentStatus;
+
+
+    constructor(data?: Partial<CommissionPayment>) {
+        super(data);
+    }
+}
+
+export interface CommissionPaymentRelations {
+    // describe navigational properties here
+}
+
+export type CommissionPaymentWithRelations = CommissionPayment & CommissionPaymentRelations;
