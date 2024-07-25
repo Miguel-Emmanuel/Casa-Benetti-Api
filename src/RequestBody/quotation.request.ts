e06948487c6099ca165e9809bcf20a8a33a24ca0
Waldo Espinoza <waldo@whathecode.com>
2024-07-25 11:45:49 -0600

    stock en procutos cotizacion

--- src/RequestBody/quotation.request.ts
+++ src/RequestBody/quotation.request.ts
@@ -168,7 +168,7 @@ export const CreateRequestBody: Partial<RequestBodyObject> = {
                     },
                     products: {
                         type: 'array',
-                        items: getModelSchemaRef(QuotationProductsCreate, {exclude: ['id', 'createdAt', 'status', 'quotationId', 'SKU', 'brandId', 'proformaId', 'dateReservationDays', 'isNotificationSent']})
+                        items: getModelSchemaRef(QuotationProductsCreate, {exclude: ['id', 'createdAt', 'status', 'quotationId', 'SKU', 'brandId', 'proformaId', 'dateReservationDays', 'isNotificationSent', 'stock']})
                         // {
                         //     properties: {
                         //         productId: {
