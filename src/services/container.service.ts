029eb832c40641991b43fdc53c1a4cf474d75b4e
Waldo Espinoza <waldo@whathecode.com>
2024-08-22 18:33:18 -0600

    Carta traduccion

--- src/services/container.service.ts
+++ src/services/container.service.ts
@@ -1,14 +1,18 @@
 import { /* inject, */ BindingScope, inject, injectable} from '@loopback/core';
 import {Filter, FilterExcludingWhere, InclusionFilter, repository} from '@loopback/repository';
+import {HttpErrors, Response, RestBindings} from '@loopback/rest';
 import dayjs from 'dayjs';
 import ExcelJS from 'exceljs';
+import fs from "fs/promises";
+import path from 'path';
 import {ContainerStatus} from '../enums';
 import {Docs, PurchaseOrdersContainer, UpdateContainer, UpdateContainerProducts} from '../interface';
 import {schemaCreateContainer, schemaUpdateContainer, schemaUpdateContainerProduct} from '../joi.validation.ts/container.validation';
-import {ResponseServiceBindings} from '../keys';
+import {ResponseServiceBindings, STORAGE_DIRECTORY} from '../keys';
 import {Container, ContainerCreate, Document, PurchaseOrders, PurchaseOrdersRelations, QuotationProducts, QuotationProductsWithRelations} from '../models';
 import {ContainerRepository, DocumentRepository, PurchaseOrdersRepository, QuotationProductsRepository} from '../repositories';
 import {ResponseService} from './response.service';
+
 @injectable({scope: BindingScope.TRANSIENT})
 export class ContainerService {
     constructor(
@@ -22,6 +26,9 @@ export class ContainerService {
         public quotationProductsRepository: QuotationProductsRepository,
         @repository(PurchaseOrdersRepository)
         public purchaseOrdersRepository: PurchaseOrdersRepository,
+        @inject(STORAGE_DIRECTORY) private storageDirectory: string,
+        @inject(RestBindings.Http.RESPONSE)
+        public res: Response,
     ) { }
 
     async create(container: Omit<ContainerCreate, 'id'>,) {
@@ -44,7 +51,40 @@ export class ContainerService {
                     scope: {
                         include: [
                             {
-                                relation: 'quotationProducts'
+                                relation: 'quotationProducts',
+                                scope: {
+                                    include: [
+                                        {
+                                            relation: 'provider'
+                                        },
+                                        {
+                                            relation: 'brand'
+                                        },
+                                        {
+                                            relation: 'quotation',
+                                            scope: {
+                                                include: [
+                                                    {
+                                                        relation: 'customer'
+                                                    }
+                                                ]
+                                            }
+                                        },
+                                        {
+                                            relation: 'product',
+                                            scope: {
+                                                include: [
+                                                    {
+                                                        relation: 'line'
+                                                    },
+                                                    {
+                                                        relation: 'document'
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    ]
+                                }
                             }
                         ]
                     }
@@ -56,30 +96,89 @@ export class ContainerService {
         const worksheet = workbook.addWorksheet('Carta traducción');
 
         const columns = [
-            {header: 'Monto', key: 'monto', width: 20},
-            {header: 'Num Banco', key: 'numBanco', width: 20},
-            {header: 'Nombre Banco', key: 'nombreBanco', width: 20},
+            {header: 'Código', key: 'codigo', width: 20},
+            {header: 'Número de factura', key: 'numeroFactura', width: 20},
+            {header: 'Id de orden de compra', key: 'ordenCompraId', width: 20},
+            {header: 'Proveedor', key: 'proveedor', width: 20},
+            {header: 'Marca', key: 'marca', width: 20},
+            {header: 'Cliente', key: 'cliente', width: 20},
+            {header: 'Linea', key: 'linea', width: 20},
+            {header: 'Modelo', key: 'modelo', width: 20},
+            {header: 'Cantidad', key: 'cantidad', width: 20},
+            {header: 'Precio unitario Euros', key: 'precioUnitarioEuros', width: 20},
+            {header: 'Precio total', key: 'precioTotal', width: 20},
+            {header: 'Fotografía', key: 'fotografia', width: 20},
+            {header: 'Descripción', key: 'descripcion', width: 20},
+            {header: 'Peso neto', key: 'pesoNeto', width: 20},
+            {header: 'Peso bruto', key: 'pesoBruto', width: 20},
+            {header: 'País de origen', key: 'paisOrigen', width: 20},
         ];
         const startRow = 2;
         worksheet.getRow(startRow).values = columns.map(column => column.header);
         worksheet.columns = columns;
 
-        worksheet.mergeCells('A1:J1');
+        worksheet.mergeCells('A1:P1');
         worksheet.getCell('A1').value = 'Carta traducción';
         worksheet.getCell('A1').alignment = {vertical: 'middle', horizontal: 'center'};
 
-        const data = [{a: 'hola', b: 'bebe', c: 'jajajaja'}]
+        for (let index = 0; index < container?.purchaseOrders?.length; index++) {
+            const element = container?.purchaseOrders[index];
+            for (let index = 0; index < element?.quotationProducts?.length; index++) {
+                const elementProduct = element?.quotationProducts[index];
+                const {product, mainMaterial, mainFinish, secondaryMaterial, secondaryFinishing, } = elementProduct;
+                const {line, name} = product;
+                const descriptionParts = [
+                    line?.name,
+                    name,
+                    mainMaterial,
+                    mainFinish,
+                    secondaryMaterial,
+                    secondaryFinishing
+                ];
+                const description = descriptionParts.filter(part => part !== null && part !== undefined && part !== '').join(' ');
+                let file = ''
+                try {
+                    const fileName = elementProduct?.product?.document?.fileURL?.split('/').pop();
+                    if (fileName) {
+                        const localFile = this.validateFileName(fileName);
+                        console.log(localFile)
+                        file = `data:image/svg+xml;base64,${await fs.readFile(localFile, {encoding: 'base64'})}`
+                    }
+                } catch (error) {
 
-        data.forEach((row: any) => {
-            const newRow = worksheet.addRow({
-                monto: row.a,
-                numBanco: row.b,
-                nombreBanco: row.c,
-            });
-        });
+                }
+                const newRow = worksheet.addRow({
+                    codigo: elementProduct?.SKU,
+                    numeroFactura: elementProduct?.invoiceNumber,
+                    ordenCompraId: element.id,
+                    proveedor: elementProduct?.provider?.name,
+                    marca: elementProduct?.brand?.brandName,
+                    cliente: elementProduct?.quotation?.customer?.name,
+                    linea: elementProduct?.product?.line?.name,
+                    modelo: elementProduct?.model,
+                    cantidad: elementProduct?.quantity,
+                    precioUnitarioEuros: elementProduct?.originCost,
+                    precioTotal: elementProduct?.price,
+                    fotografia: file,
+                    descripcion: description,
+                    pesoNeto: elementProduct?.netWeight,
+                    pesoBruto: elementProduct?.grossWeight,
+                    paisOrigen: elementProduct?.product?.countryOrigin,
+                });
 
-        const filename = `test`;
-        await workbook.xlsx.writeFile(`.sandbox/${filename}.xlsx`);
+            }
+
+        }
+        const buffer = await workbook.xlsx.writeBuffer();
+        this.res.setHeader('Content-Disposition', `attachment; filename=archivo-carta-traduccion.xlsx`);
+        this.res.setHeader('Content-Type', 'application/xlsx');
+        return this.res.status(200).send(buffer)
+    }
+
+    private validateFileName(fileName: string) {
+        const resolved = path.resolve(this.storageDirectory, fileName);
+        if (resolved.startsWith(this.storageDirectory)) return resolved;
+        throw new HttpErrors.BadRequest(`Invalid file name: ${fileName}`);
     }
 
     async updateById(id: number, data: UpdateContainer,) {
